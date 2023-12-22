4465bd9d60d481791a2384414cabd26c0bdabea0
klawrynowicz <97909990+klawrynowicz@users.noreply.github.com>
2023-12-22 09:15:26 +0100

    new

--- CDVAZureFN/PeopleFn.cs
+++ CDVAZureFN/PeopleFn.cs
@@ -1,16 +1,15 @@
-using System.ComponentModel;
 using System.Net;
-using System.Text.Json;
-using Azure;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace CdvAzure.Functions
 {
     public class PeopleFn
     {
         private readonly ILogger _logger;
+
         private readonly PeopleService peopleService;
 
         public PeopleFn(ILoggerFactory loggerFactory, PeopleService peopleService)
@@ -20,49 +19,42 @@ namespace CdvAzure.Functions
         }
 
         [Function("PeopleFn")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", "put", "delete")] HttpRequestData req)
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
+            var response = req.CreateResponse(HttpStatusCode.OK);   
 
             switch (req.Method)
             {
                 case "POST":
-                {
-                    StreamReader reader = new StreamReader(req.Body, System.Text.Encoding.UTF8);
-                    var json = reader.ReadToEnd();
-                    var person = JsonSerializer.Deserialize<Person>(json);
-                    var res = peopleService.Add(person.FirstName, person.LastName);
-                    response.WriteAsJsonAsync(res);
-                    break;
-                }
+                StreamReader reader = new StreamReader(req.Body, System.Text.Encoding.UTF8);
+                var json = reader.ReadToEnd();
+                var person = JsonSerializer.Deserialize<Person>(json);
+                var res = peopleService.Add(person.FirstName, person.LastName);
+                response.WriteAsJsonAsync(res);
+                break;
                 case "PUT":
-                {
-                    StreamReader reader = new StreamReader(req.Body, System.Text.Encoding.UTF8);
-                    var json = reader.ReadToEnd();
-                    var person = JsonSerializer.Deserialize<Person>(json);
-                    var res = peopleService.Add(person.FirstName, person.LastName);
-                    response.WriteAsJsonAsync(res);
-                    break;
-                }
+                StreamReader putReader = new StreamReader(req.Body, System.Text.Encoding.UTF8);
+                var putJson = putReader.ReadToEnd();
+                var updatedPerson = JsonSerializer.Deserialize<Person>(putJson);
+                var updated = peopleService.Update(updatedPerson.Id, updatedPerson.FirstName, updatedPerson.LastName);
+                response.WriteAsJsonAsync(updated);
+                break;
                 case "GET":
-                {
-                    var people = peopleService.Get();
-                    response.WriteAsJsonAsync(people);
-                    break;
-                }
+                var people = peopleService.Get();
+                response.WriteAsJsonAsync(people);
+                break;
                 case "DELETE":
-                {
-                    StreamReader reader = new StreamReader(req.Body, System.Text.Encoding.UTF8);
-                    var json = reader.ReadToEnd();
-                    var person = JsonSerializer.Deserialize<Person>(json);
-                    var res = peopleService.Add(person.FirstName, person.LastName);
-                    response.WriteAsJsonAsync(res);
-                    break;
-                }
+                StreamReader deleteReader = new StreamReader(req.Body, System.Text.Encoding.UTF8);
+                var deleteJson = deleteReader.ReadToEnd();
+                var personToDelete = JsonSerializer.Deserialize<Person>(deleteJson);
+                peopleService.Delete(personToDelete.Id);
+                response.WriteString("Person deleted successfully");
+                break;
             }
-          
+
+
             return response;
         }
     }
--- CDVAZureFN/PeopleService.cs
+++ CDVAZureFN/PeopleService.cs
@@ -1,53 +1,80 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace CdvAzure.Functions
 {
-    public class PeopleService
+
+    public class PersonContext : DbContext
     {
-        public List<Person> people { get; } = new List<Person>();
-        
-        public Person Add(string firstName, string lastName)
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var person = new Person
-            {
-                FirstName = firstName,
-                LastName = lastName,
-                Id = people.Count + 1
-            };
-            
-            people.Add(person);
-            return person;
+            optionsBuilder.UseSqlServer("Server=tcp:cdvdbserver1.database.windows.net,1433;Initial Catalog=cdvdbserver;Persist Security Info=False;User ID=kacper;Password=MySuperPassword12#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
-        
-        public Person Update(int id, string firstName, string lastName)
+    }
+
+    public class PeopleService
+{
+    private readonly PersonContext _context;
+
+    public PeopleService()
+    {
+        _context = new PersonContext();
+    }
+
+    public Person Add(string firstName, string lastName)
+    {
+        var person = new Person
         {
-            var person = people.First(w => w.Id == id);
+            FirstName = firstName,
+            LastName = lastName
+        };
+
+        _context.People.Add(person);
+        _context.SaveChanges();
+
+        return person;
+    }
+
+    public Person Update(int id, string firstName, string lastName)
+    {
+        var person = _context.People.FirstOrDefault(w => w.Id == id);
+        if (person != null)
+        {
             person.FirstName = firstName;
             person.LastName = lastName;
-            
-            return person;
+
+            _context.SaveChanges();
         }
-        
-        public void Delete(int id)
-        {
-            var person = people.First(w => w.Id == id);
-            people.Remove(person);
-        }
-        
-        public Person Find(int id)
-        {
-            return people.First(w => w.Id == id);
-        }
-        
-        public IEnumerable<Person> Get()
-        {
-            return people;
-        }
+
+        return person;
     }
 
-
-    public class Person
+    public void Delete(int id)
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        var person = _context.People.FirstOrDefault(w => w.Id == id);
+        if (person != null)
+        {
+            _context.People.Remove(person);
+            _context.SaveChanges();
+        }
     }
+
+    public Person Find(int id)
+    {
+        return _context.People.FirstOrDefault(w => w.Id == id);
+    }
+
+    public IEnumerable<Person> Get()
+    {
+        return _context.People.ToList();
+    }
+}
+
+        public class Person
+        {
+            public int Id {get; set; }
+            public string FirstName {get; set; }
+            public string LastName { get; set; }
+        }
 }
